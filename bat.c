0db346327ab08a3b2c8dc904a0362128e0f43a98
Raph <125955477+RaphielSh@users.noreply.github.com>
2024-01-17 18:12:27 +0300

    Syntax enhancements

--- bat.c
+++ bat.c
@@ -2,36 +2,33 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-
 int main(){
-
 chdir("/sys/class/power_supply/BAT0/");
 
-int batfull;
-int batnow;
-int fin;
+int batfull, batnow, fin;
 
 char *bfull = "charge_full";
 char *bnow = "charge_now";
-///////////////////////////File open///////////////////////////////////
+	
+//File open
 FILE* fp = fopen(bfull, "r");
 FILE* fp2 = fopen(bnow, "r");
-////////////////////////////Check if it opened/////////////////////////
+	
+//Check if it opened
 if (NULL == fp || NULL == fp2){
 	printf("Something went wrong\n");
 	return 1;
 }
-/////////////////////////////Fill array///////////////////////////////
+//Fill array
 fscanf(fp, "%d", &batfull);
 fscanf(fp2, "%d", &batnow);
-/////////////////////////////////////////////////////////////////////
 
 fin = (batnow * 100) / batfull;
 
-///////////////////////////////Print array///////////////////////////
-
+//Print an array
 printf("%d%%\n", fin);
-/////////////////////////File close/////////////////////////////////
+	
+//File close
 fclose(fp);
 fclose(fp2);
 
